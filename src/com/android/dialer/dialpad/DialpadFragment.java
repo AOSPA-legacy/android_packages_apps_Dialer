c25845c7ca60e3c15b43f2e8878626f876b4b97b
Yanuar Harry <ai.the.smarties.physics@gmail.com>
2014-03-15 12:12:51 +0700

    [3/4] Dialer: Smart Phone Call
    Put on your ear, and this will automatically call current number in message or dialer
    
    Change-Id: I0a8832e503f0acb7bf55a5a48bf7bd8e8d5da1bf

--- src/com/android/dialer/dialpad/DialpadFragment.java
+++ src/com/android/dialer/dialpad/DialpadFragment.java
@@ -87,6 +87,7 @@ import com.android.dialer.SpeedDialUtils;
 import com.android.dialer.SpeedDialListActivity;
 import com.android.dialer.database.DialerDatabaseHelper;
 import com.android.dialer.interactions.PhoneNumberInteraction;
+import com.android.dialer.util.MultiSensorManager;
 import com.android.dialer.util.OrientationUtil;
 import com.android.internal.telephony.ITelephony;
 import com.android.internal.telephony.MSimConstants;
@@ -106,7 +107,7 @@ public class DialpadFragment extends Fragment
         implements View.OnClickListener,
         View.OnLongClickListener, View.OnKeyListener,
         AdapterView.OnItemClickListener, TextWatcher,
-        PopupMenu.OnMenuItemClickListener,
+        PopupMenu.OnMenuItemClickListener, MultiSensorManager.MultiSensorListener,
         DialpadKeyButton.OnPressedListener {
     private static final String TAG = DialpadFragment.class.getSimpleName();
     private Context mContext;
@@ -224,6 +225,8 @@ public class DialpadFragment extends Fragment
     private View mDialpad;
     private View mSpacer;
 
+    private MultiSensorManager mMultiSensorManager;
+
     /**
      * Set of dialpad keys that are currently being pressed
      */
@@ -321,6 +324,12 @@ public class DialpadFragment extends Fragment
         return (TelephonyManager) getActivity().getSystemService(Context.TELEPHONY_SERVICE);
     }
 
+    private boolean getSmartCallEnabled() {
+        int smartCallEnabled = Settings.AOSPA.getInt(getActivity().getContentResolver(),
+                          Settings.AOSPA.SMART_PHONE_CALLER, 0);
+        return (smartCallEnabled != 0);
+    }
+
     @Override
     public void beforeTextChanged(CharSequence s, int start, int count, int after) {
         mWasEmptyBeforeTextChange = TextUtils.isEmpty(s);
@@ -380,6 +389,8 @@ public class DialpadFragment extends Fragment
         if (state != null) {
             mDigitsFilledByIntent = state.getBoolean(PREF_DIGITS_FILLED_BY_INTENT);
         }
+
+        mMultiSensorManager = new MultiSensorManager(getActivity().getApplicationContext(), this);
     }
 
     @Override
@@ -797,6 +808,10 @@ public class DialpadFragment extends Fragment
         stopWatch.lap("bes");
 
         stopWatch.stopAndLog(TAG, 50);
+
+        if (!phoneIsInUse() && getSmartCallEnabled()) {
+            mMultiSensorManager.enable();
+        }
     }
 
     @Override
@@ -821,6 +836,10 @@ public class DialpadFragment extends Fragment
         mLastNumberDialed = EMPTY_NUMBER;  // Since we are going to query again, free stale number.
 
         SpecialCharSequenceMgr.cleanup();
+
+        if (getSmartCallEnabled()) {
+            mMultiSensorManager.disable();
+        }
     }
 
     @Override
@@ -830,6 +849,19 @@ public class DialpadFragment extends Fragment
         if (mClearDigitsOnStop) {
             mClearDigitsOnStop = false;
             clearDialpad();
+        }
+    }
+
+    @Override
+    public void onPickup() {
+        if (!isDigitsEmpty()) {
+            mMultiSensorManager.disable();
+            final String number = mDigits.getText().toString();
+            final Intent intent = CallUtil.getCallIntent(number,
+                   (getActivity() instanceof DialtactsActivity ?
+                            ((DialtactsActivity) getActivity()).getCallOrigin() : null));
+            startActivity(intent);
+            hideAndClearDialpad();
         }
     }
 
